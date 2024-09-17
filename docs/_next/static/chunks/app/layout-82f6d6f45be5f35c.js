b9e06a93c1a390ee6585f03038a6e06c81b03c7f
szczebel <szczebel@yahoo.com>
2024-09-17 18:30:18 +0200

    wip

--- docs/_next/static/chunks/app/layout-82f6d6f45be5f35c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8980:function(n,e,u){Promise.resolve().then(u.t.bind(u,1048,23))},1048:function(){}},function(n){n.O(0,[141,971,23,744],function(){return n(n.s=8980)}),_N_E=n.O()}]);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import logo from "@/assets/logo.png"
 
 export default function Home() {
   return (
-    <Image src={logo} alt="logo" style={{margin:"100px 5% auto 5%", width:"90%"}}/>
+    <img src={logo.src} alt="logo" style={{margin:"100px 5% auto 5%", width:"90%"}}/>
   )
 }
